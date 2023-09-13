bdd7c76563860cde37c63647b2b2f37f20ec2ed1
shayansheikhs <shayanshaikh530@gmail.com>
2023-09-13 21:47:30 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,87 @@
+ /*var studentname = "shayan"
+var  arraystudentname =["shayan"]
+console.log(  arraystudentname)
+
+var  arraystudentname =["s", "h", "a" ,"y", "a", "n"]
+console.log(  arraystudentname)
+
+
+var  arraystudentname =["1", "2", "3" ,"4", "5", "6"]
+console.log(  arraystudentname)
+
+var  arraystudentname =["=", "<", "<"]
+console.log(  arraystudentname)
+
+var  arraystudentname =["1", "2", "3" ,"b", "a", "n"]
+console.log(  arraystudentname)
+ 
+var qulaity =[ "QUALIFICATION"]
+console.log( qulaity)
+console.log(  
+    "ssc /n" +"HSC /n" + "BCS /n" +"BS /n" +"BCOM /n"+"MS /n" +"M.PHIL /n" +"PHD" 
+ )*/
+/*
+var arraystudentname = ["Michael"]
+var arraystudentname1 = ["John"]
+var arraystudentname2= ["Tony"]
+ var  arraytotalmark =["500"]
+var arrryMichaeloptionalmark=["320"]
+var  arrryJohnoptionalmark=["3230"]
+var  arrryTonyoptionalmark=["480"]
+const sum =arrryMichaeloptionalmark /arraytotalmark
+const sum1 = sum*100
+document.write( "<b>"+ "Score of "+arraystudentname + " is " + arrryMichaeloptionalmark + ": " + sum1 +"%" + "<b>")
+const sum2 =arrryJohnoptionalmark /arraytotalmark
+const sum3 = sum2*100
+document.write(  "</br>"+"<b>"+ "Score of "+arraystudentname1 + " is " + arrryJohnoptionalmark + ": " + sum3 +"%" + "<b>")
+const sum4 =arrryJohnoptionalmark /arraytotalmark
+const sum5 = sum4*100
+document.write(  "</br>"+"<b>"+ "Score of "+arraystudentname2 + " is " + arrryTonyoptionalmark + ": " + sum5 +"%" + "<b>")
+*/
+/*
+const point  =[320,230,480,120]
+document.write( "<br>"+  "Score of Student :"+point)
+point. sort (function(a,b){return(a-b)})
+document.write( "<br>"+ " Order Score of Student :"+ point)
+const point1  =["Keyboard", "mouse","printer","monitor"]
+document.write( "<br>"+  "Score of Student :"+point1)
+point1. sort (function(a,b){return(a-b)})
+document.write( "<br>"+ " Order Score of Student :"+ point1)
+ */
+/*
+var arrayselect =[ "Karachi","lahore", "Peshawar", "Islamabad,Quetta"]
+document.write("<br>" +   "Cities Lists :" +"<br>"+ arrayselect)
+ var newarray = arrayselect.slice( 3)
+document.write( "<br>"+" Selected Cities Lists :"+"<br>" + newarray)
+*/
+
+ 
+/*
+const array =[ "This", "is" ,"my" ,"cat"]
+ document.write( "<br>"+  "Array :" +"<br>"+  array)
+ const array1 =[ "This is my cat"]
+ document.write( "<br>"+  "String :" +"<br>"+ array1)
+ */
+ 
+const  point1= ["Keyboard", "mouse","printer","monitor"]
+document.write( "<br>" +  "Device: "+ "<br>" + point1)
+ var newarray1 =point1.slice(0,1)
+document.write( "<br>" +"out: " + "<br>" +newarray1)
+ var newarray2 =point1.slice(1,2)
+document.write( "<br>" +"out: " + "<br>" +newarray2)
+ var newarray3 =point1.slice(2,3)
+document.write( "<br>" +"out: " + "<br>" +newarray3)
+ var newarray4 =point1.slice(3,4)
+document.write( "<br>" +"out: " + "<br>" +newarray4)
+
+
+const  point2= ["Keyboard", "mouse","printer","monitor"]
+document.write( "<br>" +  "Device: "+ "<br>" + point2)
+var newarray5 =point1.slice(3,4)
+document.write( "<br>" +"out: " + "<br>" +newarray5)
+var newarray6 =point1.slice(2,3)
+document.write( "<br>" +"out: " + "<br>" +newarray6)
+var newarray7 =point1.slice(1,2)
+document.write( "<br>" +"out: " + "<br>" +newarray7)
+var newarray8 =point1.slice(0,1)
+document.write( "<br>" +"out: " + "<br>" +newarray8)
